2b9d94720707803ef6318d2c3db4bee7d80f86a3
cypressen <2050038998@qq.com>
2024-04-11 17:24:27 +0800

    still confused about what I've done

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,25 +5,25 @@
  */
 public class Collatz {
 
-    /**
-     * Buggy implementation of nextNumber!
-     */
-    public static int nextNumber(int n) {
-        if (n % 2 == 0) {
-            return n / 2;
-        } else return 3 * n + 1;
-    }
-//
-//    /** Buggy implementation of nextNumber! */
+//    /**
+//     * Buggy implementation of nextNumber!
+//     */
 //    public static int nextNumber(int n) {
-//        if (n  == 128) {
-//            return 1;
-//        } else if (n == 5) {
-//            return 3 * n + 1;
-//        } else {
-//            return n * 2;
-//        }
+//        if (n % 2 == 0) {
+//            return n / 2;
+//        } else return 3 * n + 1;
 //    }
+//
+    /** Buggy implementation of nextNumber! */
+    public static int nextNumber(int n) {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
+    }
 
     public static void main(String[] args) {
         int n = 5;
